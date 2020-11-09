465b192c3cb99b56fe319399944c6a6f286f565a
Abhijnan-Bajpai <57059194+Abhijnan-Bajpai@users.noreply.github.com>
2020-11-09 11:17:49 +0530

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import {CssBaseline} from '@material-ui/core';
-import Header from 'C:/Users/abhij/react-navbar/src/components/Header.js';
+import Header from '../components/Header';
 const useStyles = makeStyles((theme)=>({
   root : {
     minHeight: '100vh',
